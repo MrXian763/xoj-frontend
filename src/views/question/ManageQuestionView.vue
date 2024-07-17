59e1db88c2b4016d7b6b6f1a698577ace353009c
xianziye <xianzhiyong763@gmail.com>
2024-07-17 11:29:54 +0800

    题目管理页面开发

---
+++ src/views/question/ManageQuestionView.vue
@@ -0,0 +1,159 @@
+<template>
+  <div id="manageQuestionView">
+    <h2>题目管理</h2>
+    <a-table
+      :columns="columns"
+      :data="dataList"
+      :pagination="{
+        showTotal: true,
+        pageSize: searchParams.pageSize,
+        current: searchParams.pageNum,
+        total,
+      }"
+    >
+      <template #optional="{ record }">
+        <a-space>
+          <a-button type="primary" @click="doUpdate(record)">修改</a-button>
+          <a-popconfirm
+            content="确认删除该项吗？"
+            type="warning"
+            @ok="doDelete(record)"
+          >
+            <a-button status="danger">删除</a-button>
+          </a-popconfirm>
+        </a-space>
+      </template>
+    </a-table>
+  </div>
+</template>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { Question, QuestionControllerService } from "../../../generated";
+import message from "@arco-design/web-vue/es/message";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const dataList = ref([]);
+const total = ref(0);
+const searchParams = ref({
+  pageSize: 10,
+  pageNum: 1,
+});
+
+onMounted(async () => {
+  await loadData();
+});
+
+/**
+ * 加载数据
+ */
+const loadData = async () => {
+  const res = await QuestionControllerService.listQuestionByPageUsingPost(
+    searchParams.value
+  );
+  if (res.code === 0) {
+    dataList.value = res.data.records;
+    total.value = res.data.total;
+  } else {
+    message.error("加载失败" + res.message);
+  }
+};
+
+/**
+ * 删除题目
+ * @param question 题目id
+ */
+const doDelete = async (question: Question) => {
+  const res = await QuestionControllerService.deleteQuestionUsingPost({
+    id: question.id,
+  });
+  if (res.code === 0) {
+    message.success("删除成功");
+    await loadData();
+  } else {
+    message.error("删除失败" + res.message);
+  }
+};
+
+/**
+ * 更新题目
+ * @param question 题目id
+ */
+const doUpdate = (question: Question) => {
+  router.push({
+    path: "/update/question",
+    query: {
+      id: question.id,
+    },
+  });
+};
+
+const columns = [
+  {
+    title: "id",
+    dataIndex: "id",
+  },
+  {
+    title: "标题",
+    dataIndex: "title",
+  },
+  {
+    title: "难度",
+    dataIndex: "difficulty",
+  },
+  {
+    title: "标签",
+    dataIndex: "tags",
+  },
+  {
+    title: "创建者id",
+    dataIndex: "userId",
+  },
+  {
+    title: "提交数",
+    dataIndex: "subMitNum",
+  },
+  {
+    title: "通过数",
+    dataIndex: "acceptedNum",
+  },
+  {
+    title: "点赞数",
+    dataIndex: "thumbNum",
+  },
+  {
+    title: "收藏数",
+    dataIndex: "favourNum",
+  },
+  {
+    title: "答案",
+    dataIndex: "answer",
+  },
+  {
+    title: "测试用例",
+    dataIndex: "judgeCase",
+  },
+  {
+    title: "判题配置",
+    dataIndex: "judgeConfig",
+  },
+  {
+    title: "创建时间",
+    dataIndex: "createTime",
+  },
+  {
+    title: "更新时间",
+    dataIndex: "updateTime",
+  },
+  {
+    title: "操作",
+    slotName: "optional",
+  },
+];
+</script>
+
+<style scoped>
+#ManageQuestionView {
+}
+</style>
